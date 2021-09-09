0da3952ca88adef776913cd6d7d3718cdd2e45ef
mrandolph83 <mrandolph83@gmail.com>
2021-09-09 14:39:35 -0400

    Assignment complete

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 // write your createStore function here
+function createStore(candyReducer) {
+  let state;
+  
+  function dispatch(action) {
+    state = candyReducer(state, action);
+    render();
+  }
+  
+  function getState() {
+    return state
+  }
+  
+  return { dispatch, getState};
+  };
+
 
 function candyReducer(state = [], action) {
   switch (action.type) {
@@ -8,6 +23,11 @@ function candyReducer(state = [], action) {
       return state;
   }
 }
+
+let store = createStore(candyReducer);
+
+store.dispatch({ type: '@@INIT' });
+
 
 function render() {
   let container = document.getElementById('container');
